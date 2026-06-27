db47957d454ebacd8dc9a2657e168917488c29f0
DeusTonitrus <niccolomarcone7@gmail.com>
2026-06-27 16:09:49 +0200

    Delete 11-init.js

--- 11-init.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/* --- avvio con ripristino --- */
-var _restored=false;
-try{_restored=loadSaved();}catch(e){}
-renderApp();
-updateKeyDisplay();
-if(_restored)setTimeout(function(){flash(T("restored"));},300);
